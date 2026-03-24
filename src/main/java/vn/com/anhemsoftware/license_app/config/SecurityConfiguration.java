8a93bfc57ac58695373abea8519b96f35ce2c049
coggi2996 <bidang.tuyloan@gmail.com>
2026-03-24 10:53:23 +0700

    feat: Add Spring Boot Actuator with health endpoint exposure and security exclusion.

--- src/main/java/vn/com/anhemsoftware/license_app/config/SecurityConfiguration.java
+++ src/main/java/vn/com/anhemsoftware/license_app/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -39,6 +40,11 @@ public class SecurityConfiguration {
                         "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/actuator/health");
     }
 
     @Bean

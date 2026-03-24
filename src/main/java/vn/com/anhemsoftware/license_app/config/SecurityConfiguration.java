47caaa083bd67632678d5fd6535d977e928b32bb
coggi2996 <bidang.tuyloan@gmail.com>
2026-03-24 15:52:07 +0700

    feat:  allowing WebSocket access.

--- src/main/java/vn/com/anhemsoftware/license_app/config/SecurityConfiguration.java
+++ src/main/java/vn/com/anhemsoftware/license_app/config/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,9 +14,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import vn.com.anhemsoftware.license_app.filter.JwtAuthenticationFilter;
 import vn.com.anhemsoftware.license_app.service.impl.UserServiceImpl;
 
 import java.util.List;
@@ -28,17 +29,20 @@ public class SecurityConfiguration {
     @Autowired
     private UserServiceImpl userService;
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**",
                         "/v3/api-docs/**",
                         "/swagger-ui.html",
-                        "/swagger-ui/**").permitAll()
+                        "/swagger-ui/**",
+                        "/ws/**").permitAll()
                         .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
